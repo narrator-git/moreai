bab106cde3e2fa7b39541732e7b9897bfd25dafc
narrator-git <mehrabalizadaa@gmail.com>
2025-08-09 02:38:45 +0400

    Addition to previous commit

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from more import getresponse,createlog
+from flask import Flask, render_template, request, jsonify
+from more import getresponse, createlog
 from waitress import serve
 from datetime import datetime
 import os
@@ -12,10 +12,10 @@ def midnight_checker():
     while True:
         now = datetime.now()
         if now.hour == 0 and now.minute == 0:
-            with open ("previd.txt") as p:
-                previd=p.read()
+            with open("previd.txt") as p:
+                previd = p.read()
             if previd:
-                createlog(previd)               
+                createlog(previd)
         time.sleep(60)
 
 # Запускаем проверку полночи в отдельном потоке
@@ -29,22 +29,25 @@ def index():
 
 @app.route('/chat')
 def getresp():
-    usertext=request.args.get('usertext')
+    usertext = request.args.get('usertext')
     if usertext:
         getresponse(usertext)
-    
-    # Читаем всю историю чата
+
     with open("chathistory.txt") as c:
         history = [line.split("%") for line in c.read().split("\n") if line]
-    
-    # Передаем всю историю в шаблон
+
+    # Если AJAX-запрос, возвращаем json
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return jsonify(history=history)
+
+    # Иначе рендерим страницу
     return render_template("chat.html", history=history)
 
 @app.route('/journal')
 def journal():
-    with open ("journal.txt") as j:
+    with open("journal.txt") as j:
         journ = j.read()
     return render_template('journal.html', journal=journ)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
